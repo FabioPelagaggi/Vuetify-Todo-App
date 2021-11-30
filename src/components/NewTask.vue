2d65d91480ebfda5f3e37f28fa0a3265fc9cc4de
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2021-11-30 19:41:05 -0300

    todo updated

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,67 +1,80 @@
 <template>
-  <v-row justify="center">
-    <v-dialog
-      v-model="dialog"
-      max-width="600px"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Add New Task
-        </v-btn>
-      </template>
-      <v-card>
-        <form @submit.prevent="callAddNewTask">
-          <v-card-title>
-            <span class="text-h5">New Task</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col>
-                  <v-text-field v-model="$store.state.newTask.title" class="clearable" label="Task Title *" outlined dense required></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-textarea v-model="$store.state.newTask.description" class="clearable" outlined name="input-7-4" label="Task Description" value=""></v-textarea>
-                </v-col>
-              </v-row>
-            </v-container>
-            <small>*indicates required field</small>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialog = false">
-              Close
-            </v-btn>
-            <v-btn type="submit" color="blue darken-1" text>
-              Save Task
-            </v-btn>
-          </v-card-actions>
-        </form>
-      </v-card>
-    </v-dialog>
-  </v-row>
+	<v-row justify="center">
+		<v-dialog v-model="dialog" max-width="600px">
+			<template v-slot:activator="{ on, attrs }">
+				<v-btn color="primary" dark v-bind="attrs" v-on="on">
+					Add New Task
+				</v-btn>
+			</template>
+			<v-card>
+				<form @submit.prevent="callAddNewTask">
+					<v-card-title>
+						<span class="text-h5">New Task</span>
+					</v-card-title>
+					<v-card-text>
+						<v-container>
+							<v-row>
+								<v-col>
+									<v-text-field
+										v-model="newTaskTitle"
+										class="clearable"
+										label="Task Title *"
+										outlined
+										dense
+										required
+									></v-text-field>
+								</v-col>
+								<v-col cols="12">
+									<v-textarea
+										v-model="newTaskDescription"
+										class="clearable"
+										outlined
+										name="input-7-4"
+										label="Task Description"
+										value=""
+									></v-textarea>
+								</v-col>
+							</v-row>
+						</v-container>
+						<small>*indicates required field</small>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn color="blue darken-1" text @click="dialog = false">
+							Close
+						</v-btn>
+						<v-btn type="submit" color="blue darken-1" text> Save Task </v-btn>
+					</v-card-actions>
+				</form>
+			</v-card>
+		</v-dialog>
+	</v-row>
 </template>
 
 <script>
-export default {
-    name: 'NewTasks',
-    data: () => ({
-      dialog: false,
-    }),
-    methods:{
-      callAddNewTask: function () {
-        this.$store.dispatch('addNewTask')
-        this.dialog = false
-      }
-    }
-}
+	export default {
+		name: 'NewTasks',
+		data: () => ({
+			dialog: false,
+      newTaskTitle: '',
+      newTaskDescription: ''
+		}),
+		methods: {
+			callAddNewTask: function () {
+        console.log('this.newTaskTitle', this.newTaskTitle)
+        let newTask = {
+          id: Date.now(),
+          title: this.newTaskTitle,
+          description: this.newTaskDescription,
+          done: false
+        }
+				this.$store.dispatch('addNewTask', newTask)
+				this.dialog = false
+        this.newTaskTitle = ''
+        this.newTaskDescription = ''
+			},
+		},
+	}
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,46 +2,45 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import myTasks from '../data/tasks.js'
 import mockTasks from '../data/mockTasks.js'
-import newTask from '../data/newTask.js'
+// import newTask from '../data/newTask.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    myTasks: myTasks,
-    mockTasks: mockTasks,
-    newTask: newTask,
-    doneCount: 0
-  },
-  mutations: {
-    doneTask(state, task_id){
-      let task = state.myTasks.filter(task => task.id === task_id)[0]
-      task.done = !task.done
-      window.localStorage.setItem('myTasks', JSON.stringify(state.myTasks))
-    },
-    deleteTask(state, task_id){
-      state.myTasks = state.myTasks.filter(task => task.id !== task_id)
-      window.localStorage.setItem('myTasks', JSON.stringify(state.myTasks))
-    },
-    saveNewTaskID(state){
-      state.newTask.id = Date.now()
-    },
-    saveNewTask(state){
-      state.myTasks.push(state.newTask)
-      window.localStorage.setItem('myTasks', JSON.stringify(state.myTasks))
-      window.location.reload()
-    }
-
-  },
-  actions: {
-    addNewTask(){
-      this.commit('saveNewTaskID')
-      this.commit('saveNewTask')
-    },
-  },
-  getters: {
-    getTasksDone(state){
-      return state.myTasks.filter(task => task.done).length
-    },
-  }
+	state: {
+		myTasks: myTasks,
+		mockTasks: mockTasks,
+		// newTask: newTask,
+		doneCount: 0,
+	},
+	mutations: {
+		doneTask(state, task_id) {
+			let task = state.myTasks.filter((task) => task.id === task_id)[0]
+			task.done = !task.done
+			window.localStorage.setItem('myTasks', JSON.stringify(state.myTasks))
+		},
+		deleteTask(state, task_id) {
+			state.myTasks = state.myTasks.filter((task) => task.id !== task_id)
+			window.localStorage.setItem('myTasks', JSON.stringify(state.myTasks))
+		},
+		saveNewTaskID(state) {
+			state.newTask.id = Date.now()
+		},
+		saveNewTask(state, task) {
+			state.myTasks.push(task)
+			window.localStorage.setItem('myTasks', JSON.stringify(state.myTasks))
+			// window.location.reload()
+		},
+	},
+	actions: {
+		addNewTask(context, task) {
+			// this.commit('saveNewTaskID')
+			context.commit('saveNewTask', task)
+		},
+	},
+	getters: {
+		getTasksDone(state) {
+			return state.myTasks.filter((task) => task.done).length
+		},
+	},
 })
